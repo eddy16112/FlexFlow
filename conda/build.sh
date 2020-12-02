f94eadabb9bef2cf5f2e24c238aa6914fb8c8abb
Wei Wu <eddy16112@gmail.com>
2020-12-02 02:09:21 -0700

    [Conda]: fix cublas

--- conda/build.sh
+++ conda/build.sh
@@ -7,7 +7,6 @@ export LG_RT_DIR=/home/wwu/legion-cr/runtime
 export LD_LIBRARY_PATH=/home/wwu/app/protobuf-3.11.4/lib:$LD_LIBRARY_PATH
 export PYTHON_VERSION_MAJOR=3
 export FF_ENABLE_DEBUG=1
-export DEBUG=1
+export DEBUG=0
 cd python
-
 make
